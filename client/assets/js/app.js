277e1545566a4efb49c191f8f8567b3e827a22d7
Hesham Mamdouh <hesham@egyncy.com>
2017-02-08 11:37:56 +0200

    make views and routes

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -10,6 +10,26 @@
     'foundation.dynamicRouting',
     'foundation.dynamicRouting.animations'
   ])
+  .controller('MainCtrl', function($scope, Page){
+    $scope.Page = Page;
+  })
+  .controller('HomeCtrl', function($scope, $state, $http, Page){
+    Page.setTitle('Home');
+  })
+  .controller('UsersCtrl', function($scope, $state, $http, Page){
+    Page.setTitle('Users');
+    console.log($state.params.id)
+  })
+  .controller('AboutCtrl', function($scope, $state, $http, Page){
+    Page.setTitle('About');
+  })
+  .factory('Page', function() {
+    var title = 'default';
+    return {
+      title: function() { return title; },
+      setTitle: function(newTitle) { title = newTitle }
+    };
+  })
     .config(config)
     .run(run)
   ;
@@ -17,14 +37,29 @@
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
 
   function config($urlProvider, $locationProvider) {
+
+    $urlProvider.rule(function($injector, $location) {
+
+      var path = $location.path();
+      var hasTrailingSlash = path[path.length-1] === '/';
+
+      if(hasTrailingSlash) {
+
+        //if last charcter is a slash, return the same url without the slash  
+        var newPath = path.substr(0, path.length - 1); 
+        return newPath; 
+      } 
+
+    });
+
     $urlProvider.otherwise('/');
 
     $locationProvider.html5Mode({
-      enabled:false,
-      requireBase: false
+      enabled:true,
+      rewriteLinks: false
     });
 
-    $locationProvider.hashPrefix('!');
+
   }
 
   function run() {
